93ea06585f3727141d494be7762c916e178b1a67
carol <gill20519@gmail.com>
2025-10-28 13:52:40 +0800

    Question1Completed

---
+++ CandyOne.js
@@ -0,0 +1,23 @@
+// 編號：CANDY-001
+// 程式語言：JavaScript
+// 題目：找出陣列裡最小的兩個值的總和
+// 例如：
+//   [15, 28, 4, 2, 43] 印出 6
+//   [23, 71, 33, 82, 1] 印出 24
+
+function sumOfSmallestValues(arr) {
+  //排序arr:由小到大
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  x = arr[0];
+  y = arr[1];
+  return x + y;
+}
+
+const list1 = [19, 5, 42, 2, 77];
+const list2 = [23, 15, 59, 4, 17];
+
+console.log(sumOfSmallestValues(list1)); // 印出 7
+console.log(sumOfSmallestValues(list2)); // 印出 19
